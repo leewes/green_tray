85b81710271164e232b535bd73b65ceacceb0395
Wesley <leewes5928@users.noreply.github.com>
2021-12-13 18:20:49 +0900

    Add server files.

---
+++ server/index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const app = require("./app");
+const db = require("./knex");
+
+const port = process.env.PORT || 9000;
+
+(async () => {
+    try {
+        console.log("Running migrations...");
+        await db.migrate.latest();
+        
+        console.log("Running seed files...");
+        await db.seed.run();
+
+        console.log("Starting express...");
+        app.listen(port, () => {
+            console.log(`App listening on port ${port}`);
+        });
+    } catch (err) {
+        console.error("Error starting app!", err);
+        process.exit(-1);
+    }
+})();
