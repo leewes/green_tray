00be4c43f4e27d63784003985083f4333779dbf7
Wesley <leewes5928@users.noreply.github.com>
2021-12-14 13:51:21 +0900

    Add GET and POST for /api/trays.

--- server/app.js
+++ server/app.js
@@ -14,9 +14,37 @@ app.use(
 app.use(express.static(path.resolve(__dirname, "..", "dist")));
 
 app.get("/api/seeds", async (req, res) => {
-  const seeds = await db.select().table("seeds");
-  const names = seeds.map(seed => seed.name);
-  res.status(200).json(names);
+  try {
+    const seeds = await db.select().table("seeds");
+    res.status(200).json(seeds);
+  } catch (err) {
+    console.error("Error loading seeds!", err);
+    res.sendStatus(500);
+  }
+});
+
+app.get("/api/trays", async (req, res) => {
+  try {
+    const trays = await db.select().table("trays");
+    res.status(200).json(trays);
+  } catch (err) {
+    console.error("Error loading trays!", err);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/api/trays", async (req, res) => {
+  try{
+    const data = await db.insert(req.body).returning("*").into("trays");
+    res.status(200).json(data)
+  } catch(err) {
+    console.error("Error posting tray!", err);
+    res.sendStatus(500);
+  }
+})
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "dist", "index.html"));
 });
 
 module.exports = app;
