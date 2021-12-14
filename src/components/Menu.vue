765fbba266eac4e857dcee55c7154051c0859997
Wesley <leewes5928@users.noreply.github.com>
2021-12-14 12:25:24 +0900

    Add Menu component.

---
+++ src/components/Menu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="menubox">
+      <label>
+          Tray Name:
+      <input>
+      </label>
+          <label>
+          Seed Name:
+      <input>
+      </label>
+          <label>
+          Seed Color:
+      <input>
+      </label>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Menu"
+}
+</script>
+
+<style>
+
+</style>
